589c04db6a9bc115688a95ae7706212da387a032
Siphiwo10 <siphiwoj10@gmail.com>
2024-10-18 12:02:03 +0000

    adding dif runtimes

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -4,11 +4,12 @@
 
 import asyncio
 from typing import List
-from 0-basic_async_syntax import wait_random
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """Spawn wait_random n times
-    with the specified max_delay.
-    Return the list of all delays in ascending order."""
-    delays = [await wait_random(max_delay) for _ in range(n)]await wa
-delays.appendin
+    """def for wait_n"""
+    delays = [await wait_random(max_delay) for _ in range(n)]
+    return sorted(delays)
